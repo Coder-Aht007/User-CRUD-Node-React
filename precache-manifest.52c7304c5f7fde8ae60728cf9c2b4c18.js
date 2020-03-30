23e7fe1d10038701970a700851afcc47a15c9afa
Coder-Aht007 <60315450+Coder-Aht007@users.noreply.github.com>
2020-03-30 16:22:34 +0500

    Updates

---
+++ precache-manifest.52c7304c5f7fde8ae60728cf9c2b4c18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70179e1afe4257748bc2f25d7c6dcc8e",
+    "url": "/User-CRUD-Node-React/index.html"
+  },
+  {
+    "revision": "5294602363034c8718a5",
+    "url": "/User-CRUD-Node-React/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "782c927c28dd88cd0348",
+    "url": "/User-CRUD-Node-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5294602363034c8718a5",
+    "url": "/User-CRUD-Node-React/static/js/2.2e753b15.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/User-CRUD-Node-React/static/js/2.2e753b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782c927c28dd88cd0348",
+    "url": "/User-CRUD-Node-React/static/js/main.3699b78a.chunk.js"
+  },
+  {
+    "revision": "46f7530cd863be774649",
+    "url": "/User-CRUD-Node-React/static/js/runtime-main.eada1d8f.js"
+  }
+]);
